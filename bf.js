da1184978239667fdd6b7933324cd1b791e70d4b
EhteshamAsaad <138641859+EhteshamAsaad@users.noreply.github.com>
2024-02-16 19:42:05 +1100

    Update bf.js

--- bf.js
+++ bf.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-routr-dom";
 import Layout from "./page/Laot";
 import Home from "./pages/Hoe/i";
